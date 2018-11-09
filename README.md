1c95d84fcac3c586c89cdeaa0f51a35ce715e71c
Alexandre TK <alexandretk@Alexandres-MacBook-Pro.local>
2018-11-09 20:44:47 -0200

    Library structure

---
+++ README.md
@@ -0,0 +1,8 @@
+## SUNFLOWER LOW LEVEL
+THIS IS PART OF THE SOFTWARE USED IN THE SUNFLOWER PROJECT.
+
+This library converts high level information into lower level information, used to control the system motors.
+Function call's are converted into G-Code strings.
+
+## DOWNLOAD URL:
+pip install -i https://test.pypi.org/simple/ sunflower-ll
---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sunflower_ll",
+    version="0.0.1",
+    author="sunflower team - pi2",
+    author_email="alex@sunflower.com",
+    description="sunflower low level",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/PI2-sunflower",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
